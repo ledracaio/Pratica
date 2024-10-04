8a53b24bf2e0d202debeaea13d6715c9f0ffa581
ledracaio <caioaugustoledra@gmail.com>
2024-10-04 19:08:37 -0300

    arruma

--- Aula 5/js/notas.js
+++ Aula 5/js/notas.js
@@ -28,36 +28,43 @@ function totalizaNota() {
 
       tbody.appendChild(totalRow);
   }
-  
-  function totalizaAluno() {
-    const table = document.getElementById("notas");
-    const tbody = table.tBodies[0];
-    const headerRow = table.tHead.rows[0];
-    const lastColumn = tbody.rows[tbody.rows.length - 1].cells[tbody.rows[0].cells.length - 1];
+}
 
-    const totalHeader = document.getElementById("titulo");
-    const thColuna = document.createElement("th");
-    thColuna.setAttribute("rowspan","2");
-    thColuna.innerHTML = "<b>Total</b>";
-    totalHeader.appendChild(thColuna);
-  
-    const totalColumn = document.createElement("td");
-  
-    for (let row of tbody.rows) {
-      let sum = 0;
-      let count = 0;
-  
-      for (let i = 1; i < row.cells.length; i++) {
-        const cell = row.cells[i];
-        if (cell.textContent !== "") {
-          sum += parseFloat(cell.textContent);
-          count++;
-        }
+function totalizaAluno() {
+  if (!alunoDisabled) {
+      alunoDisabled = true;
+      const table = document.getElementById("notas");
+      const tbody = table.tBodies[0];
+      const headerRow = table.tHead.rows[0];
+      const lastColumn = tbody.rows[tbody.rows.length - 1].cells[tbody.rows[0].cells.length - 1];
+
+      const totalColumn = document.createElement("td");
+      totalColumn.innerHTML = "<b>Total</b>";
+
+      const headerColumn = document.createElement("th");
+      headerColumn.setAttribute("rowspan", 2);
+      headerColumn.innerHTML = "<b>Total</b>";
+
+      headerRow.appendChild(headerColumn);
+
+      for (let row of tbody.rows) {
+          let sum = 0;
+          let count = 0;
+
+          for (let i = 1; i < row.cells.length; i++) {
+              const cell = row.cells[i];
+              if (cell.textContent !== "") {
+                  sum += parseFloat(cell.textContent);
+                  count++;
+              }
+          }
+
+          const average = sum / count;
+          row.appendChild(totalColumn.cloneNode(true));
+          row.lastChild.textContent = average.toFixed(2);
       }
-  
-      const average = sum / count;
-      row.appendChild(totalColumn.cloneNode(true));
-      row.lastChild.textContent = average.toFixed(2);
-    }
-
   }
+}
+
+let alunoDisabled = false;
+let notaDisabled = false;
